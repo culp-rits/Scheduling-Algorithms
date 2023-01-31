371982a0444124e106ff31d6acb45660e80d7687
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-01-31 15:00:30 +0530

    Update first-come-first-serve.c

--- first-come-first-serve.c
+++ first-come-first-serve.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+//header file
 
 struct process{
     int id, arrival_time, burst_time,completion_time, turnaround_time,waiting_time;
 }p[10],temp;
+//defining the structure of process
 
 int n;
+//number of process
 
 void fcfs(){
     for(int i=0;i<n;i++)
@@ -14,17 +17,23 @@ void fcfs(){
                 p[i] = p[j];
                 p[j] = temp;
             }
+    //sorting the process based on their arrival time
+    
     for (int i = 0; i < n; i++)
         if(i==0){
             p[i].completion_time = p[i].burst_time;
             p[i].turnaround_time = p[i].completion_time - p[i].arrival_time;
             p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
         }
+        //for the 1st process
+    
         else{    
             p[i].completion_time = p[i-1].completion_time + p[i].burst_time;
             p[i].turnaround_time = p[i].completion_time - p[i].arrival_time;
             p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
         }
+        //computing completion, waiting and turnaround time for the rest of the process
+    
 }
 
 void times(){
@@ -33,11 +42,15 @@ void times(){
         time+=p[i].waiting_time;
     time=time/n;
     printf("\naverage waiting time : %f",time);
+    //computing average waiting time
+    
     time=0;
     for(int i=0; i<n; i++)
         time+=p[i].turnaround_time;
     time=time/n;
     printf("\naverage turnaround time : %f",time);
+    //computing average turnaround time
+    
 }
 
 void gant(){
@@ -47,12 +60,15 @@ void gant(){
     printf("\n0");
     for(int i=0; i<n; i++)
         printf("\t%d",p[i].completion_time);
+    //printing gant chart
+    
 }
 
 void table(){
     printf("\n");
     for(int i=0;i<n;i++)
         printf("P%d\t%d\t%d\t%d\t%d\t%d\n",p[i].id,p[i].arrival_time,p[i].burst_time,p[i].completion_time,p[i].turnaround_time,p[i].waiting_time);
+    //printing the completed table
 }
 
 int main(){
@@ -66,8 +82,12 @@ int main(){
         printf("burst time : ");
         scanf("%d", &p[i].burst_time);
     }
+    //input for all process
+    
     fcfs();
     table();
     gant();
     times();
+    //prints table, gant chart and metrics
+    
 }
